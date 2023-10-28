049c5c1af45dc2317c08e848d955b9b9cc77808d
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-10-28 17:00:13 +0900

    Use argument for example ir

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include "lib/hello.h"
-#include "llvm-16/llvm/IR/Constants.h"
-#include "llvm-16/llvm/IR/LLVMContext.h"
-#include "llvm-16/llvm/IR/Verifier.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -24,19 +23,22 @@ int main() {
 
   // main function
   auto main_function = llvm::Function::Create(
-      llvm::FunctionType::get(llvm::Type::getDoubleTy(*the_context),
-                              std::vector<llvm::Type *>{}, false),
+      llvm::FunctionType::get(
+          llvm::Type::getDoubleTy(*the_context),
+          std::vector<llvm::Type *>{llvm::Type::getDoubleTy(*the_context)},
+          false),
       llvm::Function::ExternalLinkage, "main", the_module.get());
+
+  main_function->arg_begin()->setName("first_arg");
 
   auto basic_block =
       llvm::BasicBlock::Create(*the_context, "entry", main_function);
   builder->SetInsertPoint(basic_block);
 
   auto val1 = llvm::ConstantFP::get(*the_context, llvm::APFloat(1.0));
-  auto val2 = llvm::ConstantFP::get(*the_context, llvm::APFloat(2.0));
   auto val3 = llvm::ConstantFP::get(*the_context, llvm::APFloat(3.0));
 
-  auto lhs = builder->CreateFMul(val1, val2, "multmp");
+  auto lhs = builder->CreateFMul(val1, main_function->arg_begin(), "multmp");
   auto ret_val = builder->CreateFAdd(lhs, val3, "addtmp");
   builder->CreateRet(ret_val);
 
